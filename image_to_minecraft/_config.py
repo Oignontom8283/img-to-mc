28c50d4c86393070d323402aaf38b642656f418b
Oignontom8283 <tfarin83@gmail.com>
2023-10-28 15:30:38 +0200

    fix

--- image_to_minecraft/_config.py
+++
@@ -1,6 +0,0 @@
-
-__modul_version__ = '1.0.0'
-
-command_DisplayParticle = 'particle' # Minecraft command to display particles
-
-id_ParticleDisplay = 'minecraft:dust' # Identify the particle to use for display
